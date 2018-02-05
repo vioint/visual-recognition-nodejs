2b3cfede961857654f28e4397e05eb6eb8f9d3db
Vance Vu <VanceVu@ibm.com>
2018-02-05 16:12:57 -0500

    Add more info for debug

--- test/test.train-test.js
+++ test/test.train-test.js
@@ -23,6 +23,7 @@ casper.thenOpen('http://localhost:3000/train', function(result) {
   // dog type examples
   casper.waitForSelector('div._examples[data-kind="dogs"]', function() {
     console.log(JSON.stringify(this.getElementInfo('div._examples[data-kind="dogs"]')))
+    console.log(JSON.stringify(this.getElementsInfo('div._examples showing')))
 
     casper.then(function() {
       this.click('img[data-name="goldenretriever"]');
