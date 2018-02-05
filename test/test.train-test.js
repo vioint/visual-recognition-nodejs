4fa8faf2e024dbe13277848c82f77d6b635f0381
Vance Vu <VanceVu@ibm.com>
2018-02-05 14:14:13 -0500

    Add more info for debugging

--- test/test.train-test.js
+++ test/test.train-test.js
@@ -34,6 +34,7 @@ casper.thenOpen('http://localhost:3000/train', function(result) {
   casper.then(function() {
     this.test.assertSelectorHasText('button._positive[data-name="goldenretriever"]', 'Select')
     this.click('button._positive[data-name="goldenretriever"]');
+    this.test.assertSelectorHasText('button._positive[data-name="goldenretriever"]', 'Select')
     this.test.assertVisible('button._examples--class__selected[data-name="goldenretriever"]')
   });
   casper.then(function() {
