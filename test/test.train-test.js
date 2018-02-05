b5fb779119bd97d4a6e62c5af583552f64f76a97
Vance Vu <VanceVu@ibm.com>
2018-02-05 16:16:49 -0500

    Change test parameter

--- test/test.train-test.js
+++ test/test.train-test.js
@@ -21,7 +21,7 @@ casper.thenOpen('http://localhost:3000/train', function(result) {
   });
 
   // dog type examples
-  casper.waitForSelector('div._examples[data-kind="dogs"]', function() {
+  casper.waitForSelector('div._examples[data-kind="dogs"] showing', function() {
     console.log(JSON.stringify(this.getElementInfo('div._examples[data-kind="dogs"]')))
     console.log(JSON.stringify(this.getElementsInfo('div._examples showing')))
 
